c2c376da7b573739a30713ed49de33fbe25e42f8
yaredghebre <yared2391@gmail.com>
2023-12-07 17:57:56 +0100

    proj needs improvement

--- src/pages/AddNewPost.jsx
+++ src/pages/AddNewPost.jsx
@@ -162,7 +162,7 @@ const AddNewPost = () => {
             </select>
           </div> */}
 
-          <div className="mb-5">
+          {/* <div className="mb-5">
             <label
               htmlFor="tags"
               className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
@@ -178,7 +178,7 @@ const AddNewPost = () => {
               className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
               placeholder=""
             />
-          </div>
+          </div> */}
 
           <div className="mb-5">
             <label
